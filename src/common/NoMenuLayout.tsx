42e53718f4b44efc6d644e99d8392c5ad5d446b0
Eliophot <81296123+Eliophot@users.noreply.github.com>
2023-05-05 13:28:13 +0000

    Build: (2180699) Create build-site.yml

--- src/common/NoMenuLayout.tsx
+++
@@ -1,6 +0,0 @@
-import type { FC, PropsWithChildren } from 'react';
-import './NoMenuLayout.scss';
-
-export const NoMenuLayout: FC<PropsWithChildren<unknown>> = ({ children }) => (
-  <div className="no-menu-wrapper container-xl">{children}</div>
-);
--- test/utils/services/__mocks__/ColorGenerator.mock.ts
+++
@@ -1,8 +0,0 @@
-import { fromPartial } from '@total-typescript/shoehorn';
-import type { ColorGenerator } from '../../../../src/utils/services/ColorGenerator';
-
-export const colorGeneratorMock = fromPartial<ColorGenerator>({
-  getColorForKey: jest.fn(() => 'red'),
-  setColorForKey: jest.fn(),
-  isColorLightForKey: jest.fn(() => false),
-});
